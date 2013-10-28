508e14c4611d7e9d2e0feb8d0e58a84bfa6230f4
edatrix <lespath@gmail.com>
2013-10-28 17:02:20 -0600

    added test files

---
+++ test/helpers/unit_helper.rb
@@ -0,0 +1,5 @@
+require 'minitest'
+require 'minitest/autorun'
+require 'minitest/pride'
+require 'rack/test'
+require_relative '../../lib/app'
---
+++ test/unit/unit_test.rb
@@ -0,0 +1,7 @@
+require_relative '../helpers/unit_helper'
+
+
+class RestaurantAppTest < Minitest::Test
+  include Rack::Test::Methods
+
+end
