478b937ccb237b4358fd94037d1457b2cd60e3a6
Nikhil Taneja <nikhil.a.taneja@gmail.com>
2013-10-29 10:48:48 -0600

    Creates method to insert into database

--- lecentralscrape2.rb
+++ lecentralscrape2.rb
@@ -1,29 +1,23 @@
 require 'nokogiri'
 require 'open-uri'
-page = Nokogiri::HTML(open('http://www.lecentral.com/ourmenu/dinner.html'))
 require 'sequel'
 require 'sqlite3'
-# @double = []
+
+database = Sequel.sqlite('database.sqlite3')
+database.run "CREATE TABLE raw_menu_items (id integer primary key autoincrement, title varchar(255))"
+
+page = Nokogiri::HTML(open('http://www.lecentral.com/ourmenu/dinner.html'))
+
 @raw_menu_array = []
 
-# raw_menu = page.css('p > span').each do |span|
-#   @double << span.text
-# end
-database = Sequel.sqlite('database.sqlite3')
-database.run "CREATE TABLE raw_menu_items (id integer primary key autoincrement, name varchar(255))"
-#database.run "INSERT INTO raw_menu_items(name) VALUES('Lotte au Safran')"
-# database[:raw_menu_items].select.to_a
-# database.create_table :raw_menu_items do
-#     primary_key :id
-#     String      :name
-# end
+page.css('p').each do |p|
+  @raw_menu_array << p.text
+end
 
-# page.css('p').each do |p|
-#   @raw_menu_array << p.text
-# end
+@menu_items_database_table = database.from(:raw_menu_items)
 
-# @raw_menu_array.each do |raw_menu_item|
-#   database.run "INSERT INTO raw_menu_items(name) VALUES (raw_menu_item)"
-# end
+@raw_menu_array.each do |raw_menu_item|
+  @menu_items_database_table.insert(:title => raw_menu_item)
+end
 
-
+puts @menu_items_database_table.select.to_a
