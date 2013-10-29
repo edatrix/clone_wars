b6319697896189edfb8533efffde23d7f5dafa1a
Meeka <rrgayhart@gmail.com>
2013-10-29 12:22:11 -0600

    Creates all get routes and pages for basic browsing structure

--- lib/app.rb
+++ lib/app.rb
@@ -3,16 +3,28 @@ require 'sequel'
 require 'sqlite3'
 require_relative './food'
 
-#This is an attempt to speak to the database
-# database ||= Sequel.sqlite('database.sqlite3')
-# @menu_items_database_table = database[:raw_menu_items].count
-
 class RestaurantApp < Sinatra::Base
   set :method_override, true
   set :root, 'lib/app'
 
   get '/' do
     erb :index, locals: {:title => menu_items_database_table.to_a}
+  end
+
+  get '/events' do
+    erb :events
+  end
+
+  get '/menus' do
+    erb :menus
+  end
+
+  get '/party_info' do
+    erb :party_info
+  end
+
+  get '/contact' do
+    erb :contact
   end
 
   private
