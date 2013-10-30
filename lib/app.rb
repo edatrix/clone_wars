9efdf0cc409d9e9f6a280153facf5e7de46df422
Meeka <rrgayhart@gmail.com>
2013-10-29 20:34:06 -0600

    breaks out the page array logic into the db

--- lib/app.rb
+++ lib/app.rb
@@ -3,6 +3,7 @@ require 'sequel'
 require 'sqlite3'
 require_relative './food'
 require_relative './page'
+require_relative '../db/page_arrays'
 #require_relative './page_store'
 
 class RestaurantApp < Sinatra::Base
